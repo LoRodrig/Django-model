8054816e14ee2c13262e9fb9b65dcbac11d63f8e
Lorenzo Rodrigues <135261879+LoRodrig@users.noreply.github.com>
2024-04-23 05:23:55 +0000

    new commit

--- projetodjango/usuarios/urls.py
+++ projetodjango/usuarios/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('login/', views.login, name='login')
-
-
+    path ('login/', views.login, name='login'),
+    path ('plataforma/', views.plataforma, name='plataforma')
 
 ]
--- projetodjango/usuarios/views.py
+++ projetodjango/usuarios/views.py
@@ -1,5 +1,26 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    if request.method== 'GET':
+     return render(request, 'login.html')
+    else:
+       usuario = request.POST.get('usuario')
+       senha = request.POST.get('senha')
+       
+       user = authenticate(username = usuario, password = senha)
+
+       if user:
+          login(request , user)
+
+          return HttpResponse('autentificado')
+       else:
+          return HttpResponse('email ou senha invalidos')
+       
+def plataforma(request):
+   if request.user.is_authenticated:
+    return HttpResponse('plataforma')
+   return HttpResponse('Voê precisa estar logado para vizualizar')
